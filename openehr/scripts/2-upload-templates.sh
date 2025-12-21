0e2cd031dc8c734ea00c906e8e87b943597124f7
rafel <rafelr4@gmail.com>
2025-12-22 00:31:57 +0100

    feat: add radiology template

--- openehr/scripts/2-upload-templates.sh
+++ openehr/scripts/2-upload-templates.sh
@@ -3,3 +3,9 @@ curl -X POST https://base.tibbna.com/ehrbase/rest/openehr/v1/definition/template
   -H "Authorization: Basic $(echo -n 'auto-speed-ranting:KivLWsQgN4f8aiHAvwuq' | base64)" \
   -H "Content-Type: application/xml" \
   --data-binary @template_clinical_encounter_v2.opt
+
+curl -X POST https://base.tibbna.com/ehrbase/rest/openehr/v1/definition/template/adl1.4 \
+  -H "X-API-Key: BgMxGMZk5isfCWezE5CF" \
+  -H "Authorization: Basic $(echo -n 'auto-speed-ranting:KivLWsQgN4f8aiHAvwuq' | base64)" \
+  -H "Content-Type: application/xml" \
+  --data-binary @template_radiology_report_v1.opt
--- openehr/scripts/4-get-template-flat.sh
+++ openehr/scripts/4-get-template-flat.sh
@@ -1,9 +1,9 @@
-curl -X GET "https://base.tibbna.com/ehrbase/rest/openehr/v1/definition/template/adl1.4/clinical_encounter_v1/example?format=FLAT" \
+curl -X GET "https://base.tibbna.com/ehrbase/rest/openehr/v1/definition/template/adl1.4/template_clinical_encounter_v2/example?format=FLAT" \
   -H "Accept: application/json" \
   -H "X-API-Key: BgMxGMZk5isfCWezE5CF" \
   -H "Authorization: Basic $(echo -n 'auto-speed-ranting:KivLWsQgN4f8aiHAvwuq' | base64)"
 
-curl -X GET "https://base.tibbna.com/ehrbase/rest/openehr/v1/definition/template/adl1.4/template_clinical_encounter_v2/example?format=FLAT" \
+curl -X GET "https://base.tibbna.com/ehrbase/rest/openehr/v1/definition/template/adl1.4/template_radiology_report_v1/example?format=FLAT" \
   -H "Accept: application/json" \
   -H "X-API-Key: BgMxGMZk5isfCWezE5CF" \
   -H "Authorization: Basic $(echo -n 'auto-speed-ranting:KivLWsQgN4f8aiHAvwuq' | base64)"
